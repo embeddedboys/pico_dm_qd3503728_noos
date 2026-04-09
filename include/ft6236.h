887c87cb5cfae5740f6cbabcd5fe55cea908a0ec
Wooden Chair <hua.zheng@embeddedboys.com>
2026-04-09 20:03:58 +0100

    format header files
    Signed-off-by: Wooden Chair <hua.zheng@embeddedboys.com>

--- include/ft6236.h
+++ include/ft6236.h
@@ -1,5 +1,5 @@
-// Copyright (c) 2024 embeddedboys developers
-
+// Copyright (c) 2026 embeddedboys developers
+//
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
@@ -7,10 +7,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -25,32 +25,33 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define FT6236_PIN_SCL  27
-#define FT6236_PIN_SDA  26
-#define FT6236_PIN_RST  18
+#define FT6236_PIN_SCL 27
+#define FT6236_PIN_SDA 26
+#define FT6236_PIN_RST 18
 
-#define CT_MAX_TOUCH  5
+#define CT_MAX_TOUCH 5
 
-#define FT_REG_DEVICE_MODE 	    0x00    // Device mode, 0x00: Normal mode, 0x04: Test mode, 0x03: Factory mode
-#define FT_REG_GEST_ID 			0x01    // Gesture ID
-#define FT_REG_TD_STATUS 		0x02    // Touch point status
+#define FT_REG_DEVICE_MODE \
+	0x00 // Device mode, 0x00: Normal mode, 0x04: Test mode, 0x03: Factory mode
+#define FT_REG_GEST_ID	 0x01 // Gesture ID
+#define FT_REG_TD_STATUS 0x02 // Touch point status
 
 /* TODO: currently support 1 touch point */
-#define FT_REG_TOUCH1_XH 		0x03    // Touch point 1 X high 8-bit
-#define FT_REG_TOUCH1_XL 		0x04    // Touch point 1 X low 8-bit
-#define FT_REG_TOUCH1_YH 		0x05    // Touch point 1 Y high 8-bit
-#define FT_REG_TOUCH1_YL 		0x06    // Touch point 1 Y low 8-bit
+#define FT_REG_TOUCH1_XH 0x03 // Touch point 1 X high 8-bit
+#define FT_REG_TOUCH1_XL 0x04 // Touch point 1 X low 8-bit
+#define FT_REG_TOUCH1_YH 0x05 // Touch point 1 Y high 8-bit
+#define FT_REG_TOUCH1_YL 0x06 // Touch point 1 Y low 8-bit
 
-#define FT_REG_TH_GROUP			0x80
-#define FT_REG_PERIODACTIVE	    0x88
+#define FT_REG_TH_GROUP	    0x80
+#define FT_REG_PERIODACTIVE 0x88
 
-#define	FT_REG_LIB_VER_H		0xA1
-#define	FT_REG_LIB_VER_L		0xA2
-#define FT_REG_CHIPER           0xA3
-#define FT_REG_G_MODE 			0xA4
-#define FT_REG_FOCALTECH_ID     0xA8
-#define FT_REG_RELEASE_CODE_ID  0xAF
-#define FT_REG_STATE            0xBC
+#define FT_REG_LIB_VER_H       0xA1
+#define FT_REG_LIB_VER_L       0xA2
+#define FT_REG_CHIPER	       0xA3
+#define FT_REG_G_MODE	       0xA4
+#define FT_REG_FOCALTECH_ID    0xA8
+#define FT_REG_RELEASE_CODE_ID 0xAF
+#define FT_REG_STATE	       0xBC
 
 extern int ft6236_driver_init(void);
 extern bool ft6236_is_pressed(void);
--- include/ili9488.h
+++ include/ili9488.h
@@ -1,5 +1,5 @@
-// Copyright (c) 2024 embeddedboys developers
-
+// Copyright (c) 2026 embeddedboys developers
+//
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
@@ -7,10 +7,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -28,23 +28,20 @@
 #define ILI9488_X_RES LCD_HOR_RES
 #define ILI9488_Y_RES LCD_VER_RES
 
-enum {
-    LCD_ROTATE_0,
-    LCD_ROTATE_90,
-    LCD_ROTATE_180,
-    LCD_ROTATE_270
-};
-#define BIT(nr)			(1UL << (nr))
-#define MADCTL 0x36
-#define MY  BIT(7)
-#define MX  BIT(6)
-#define MV  BIT(5)
-#define ML  BIT(4)
-#define BGR BIT(3)
-#define MH  BIT(2)
+enum { LCD_ROTATE_0, LCD_ROTATE_90, LCD_ROTATE_180, LCD_ROTATE_270 };
+#define BIT(nr) (1UL << (nr))
+#define MADCTL	0x36
+#define MY	BIT(7)
+#define MX	BIT(6)
+#define MV	BIT(5)
+#define ML	BIT(4)
+#define BGR	BIT(3)
+#define MH	BIT(2)
 /* Bit 0,1 is reserved */
 
 extern int ili9488_driver_init();
-extern void ili9488_video_flush(int xs, int ys, int xe, int ye, void *vmem16, uint32_t len);
-extern void ili9488_flush(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p);
+extern void ili9488_video_flush(int xs, int ys, int xe, int ye, void *vmem16,
+				uint32_t len);
+extern void ili9488_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area,
+			  lv_color_t *color_p);
 #endif
